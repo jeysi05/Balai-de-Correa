cac3ca6e49026f7a1e281f4086a4e2e20e6b2c8f
John Alcantara <j.alcantara1005@gmail.com>
2026-04-01 19:14:32 +0800

    Secure Firebase config and use env variables

--- src/firebase.js
+++ src/firebase.js
@@ -5,13 +5,12 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAilChJ1pDoXe9MbPxieOpuMCnLurYJJ2M",
-  authDomain: "pickle-jar-app.firebaseapp.com",
-  projectId: "pickle-jar-app",
-  storageBucket: "pickle-jar-app.firebasestorage.app",
-  messagingSenderId: "974510901179",
-  appId: "1:974510901179:web:e6ddf74d52089b92c9e69e",
-  measurementId: "G-3YVYMMXLJ7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
